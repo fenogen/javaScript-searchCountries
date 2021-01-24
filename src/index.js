4442c0e03ead9a1cbe2cff883901d1d27d03b2e3
Oleg Soldatenko <sunskays@gmail.com>
2021-01-24 16:28:01 +0200

    Ver 1.6 - Добавлен плагин уведомления PNotify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import './styles.css';
 
-
-// ----------------------------> Импортирт плагина уведомлений - PNotify
-import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-
-// ----------------------------> Создали шабон разметки и импортировали его
+// ----------------------------> Создали шабоны разметки и импортировали их
 import cardTemplete from './template/card.hbs';
 import listTemplete from './template/list.hbs';
+
+// ----------------------------> Импортируем плагин уведомлений - PNotify
+import { alert, info, error, defaultModules} from '../node_modules/@pnotify/core/dist/PNotify.js';
+import "@pnotify/core/dist/PNotify.css";                     //---> Импортирует стилей
+import '@pnotify/core/dist/BrightTheme.css';                 //---> Импортирует цветовую гамму
+import 'material-design-icons/iconfont/material-icons.css'; //---> Импортирует иконки
+
+// ----------------------------> Импортируем плагин уведомлений - PNotify
+const PNotify = {
+    title: 'Info',
+    delay: 2500,          //---> Удаляется автоматически
+    sticker: false,       //---> Удаляет стиккер
+    closerHover: false,   //---> Видно иконку закрытия
+}
 
 // ----------------------------> Импортировали ф-ю debounce
 var debounce = require('lodash.debounce');
@@ -32,9 +40,9 @@ const fnTemplate = (data) => {
     
     if (data.length > 10) {
         console.log('Укажите более точно название страны');
-        defaultModules.set(PNotifyMobile, {});
-        alert({
-            text: 'Notice me, senpai!'
+        info({
+            ...PNotify,
+            text: 'Результат поиска > 10 стран. Введите более точное название страны.',
         });
         return;
     }
@@ -49,9 +57,13 @@ const fnTemplate = (data) => {
         return;
     }
 
+    error({
+        ...PNotify,
+        text: 'По вашему запросу ничего не найдено.',
+    });
+    
     return console.warn('Введите боллее корректное название')
 }
-
 
 const fnFetch = () => {
     fnFetchServer(input.value);
